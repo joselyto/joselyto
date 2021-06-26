c437cb8e775bbb78a8a8e16e951c079433421dd7
Joselyto Vasukama <vamsjose@gmail.com>
2021-06-26 08:16:12 +0100

    ajoute d'un nouveau fichier

---
+++ prise_en_charge.php
@@ -0,0 +1,125 @@
+<?php
+session_start();	
+if(isset($_SESSION['nom']))
+{
+require("BDD/bdd.php");
+require_once("head.php");
+require_once("header.php");
+require_once("menu.php");
+var_dump($_SESSION['nom']);
+$query1 = $connexion->query('SELECT * FROM resultat');
+$query1->fetchObject();
+//$nombre = $query1->fetchArray();
+//var_dump(count($nombre));
+?>
+		<!-----------------------------------Menu principal------------------------------------------------>
+
+	<div class="main-container">
+		<div class="pd-ltr-20">
+			<div class="card-box pd-20 height-100-p mb-30">
+				<div class="row align-items-center">
+					<div class="col-md-4">
+						<img src="vendors/images/ux.jpg" alt="">
+					</div>
+					<div class="col-md-8">
+						<h4 class="font-20 weight-500 mb-10 text-capitalize">
+							Welcome back <div class="weight-600 font-30 text-blue">Bienvenue au dashboard de prise en charge</div>
+						</h4>
+						<p class="font-18 max-width-600">Ici vous retrouvez une bref statistique de donn</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-3 mb-30">
+					<div class="card-box height-100-p widget-style1">
+						<div class="d-flex flex-wrap align-items-center">
+							<div class="progress-data">
+								<div id="chart"></div>
+							</div>
+							<div class="widget-data">
+								<div class="h4 mb-0"><?php //echo count($nombre); ?></div>
+								<div class="weight-600 font-14">Des enfants Drepanocytaire</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 mb-30">
+					<div class="card-box height-100-p widget-style1">
+						<div class="d-flex flex-wrap align-items-center">
+							<div class="progress-data">
+								<div id="chart2"></div>
+							</div>
+							<div class="widget-data">
+								<div class="h4 mb-0">Taux</div>
+								<div class="weight-600 font-14">Des femmes Drepanocytaire</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 mb-30">
+					<div class="card-box height-100-p widget-style1">
+						<div class="d-flex flex-wrap align-items-center">
+							<div class="progress-data">
+								<div id="chart3"></div>
+							</div>
+							<div class="widget-data">
+								<div class="h4 mb-0">Taux</div>
+								<div class="weight-600 font-14">Des femmes enceintes Drepanocytaire</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 mb-30">
+					<div class="card-box height-100-p widget-style1">
+						<div class="d-flex flex-wrap align-items-center">
+							<div class="progress-data">
+								<div id="chart4"></div>
+							</div>
+							<div class="widget-data">
+								<div class="h4 mb-0">Taux</div>
+								<div class="weight-600 font-14">des Drepanocytaires en generales</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xl-8 mb-30">
+					<div class="card-box height-100-p pd-20">
+						<h2 class="h4 mb-20">Activity</h2>
+						<div id="chart5"></div>
+					</div>
+				</div>
+				<div class="col-xl-4 mb-30">
+					<div class="card-box height-100-p pd-20">
+						<h2 class="h4 mb-20">Lead Target</h2>
+						<div id="chart6"></div>
+					</div>
+				</div>
+			</div>
+			
+		
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+	<script src="src/plugins/apexcharts/apexcharts.min.js"></script>
+	<script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+	<script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+	<script src="vendors/scripts/dashboard.js"></script>
+
+
+</body>
+</html>
+<?php
+}else{
+	?>
+	<h2>N'essayer pas d'acceder au page par URL sinon vous serez bloquer </h2>
+<?php
+}
+?>
